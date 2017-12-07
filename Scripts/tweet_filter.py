c0a50caad11a50c217d9694eef922432232bd6ec
Justin <justinwilkowski@gmail.com>
2017-12-06 22:53:15 -0800

    got basic filter for time

--- Scripts/tweet_filter.py
+++ Scripts/tweet_filter.py
@@ -1,5 +1,6 @@
 import json
 from geojson import MultiPoint as mp
+import time
 
 tweets_data = []
 tweets_file = open('../Data/twitter_data_sd.txt', "r", encoding='utf-16')
@@ -21,9 +22,12 @@ bborder = 32.6996
 for n in tweets_data:
     try:
         if n['user']['geo_enabled']:
+            print(repr(n['created_at']))
+            print(time.strftime('%H:%M:%S', time.strptime(n['created_at'],'%a %b %d %H:%M:%S +0000 %Y')))
             coord = n['coordinates']['coordinates']
             if lborder <= coord[0] <= rborder and bborder <= coord[1] <= tborder:
                 coords.append(n['coordinates']['coordinates'])
+
         valid += 1
     except TypeError:
         notvalid += 1
